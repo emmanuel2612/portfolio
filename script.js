6fa47aa4265d7c623d6cc1bf1fb0554a8eb4e606
emmanuel2612 <64177390+emmanuel2612@users.noreply.github.com>
2020-08-26 19:20:27 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,158 @@
 window.onload = () => {
 
-    var intro = document.getElementById("intro");
-    var main = document.getElementById("main");
-
-var i = 0;
-var txt = 'Developed By Emmanuel'; /* The text */
-var speed = 100; /* The speed/duration of the effect in milliseconds */
-
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("intro-text").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-
-  setTimeout(() => {
-    secondFunction();
-  },3000);
-
-}
+    var body = document.body;
+    var html = document.documentElement;
 
 
-function secondFunction (){
+    /* SHOW CONTACT */
 
-    main.style.display = "flex";
-    intro.style.display = "none";
-}
-
-
-typeWriter();
+    var contactButton = document.getElementById("contact-button");
+    var contactBox = document.getElementById("contact");
 
 
 
 
+    contactButton.onclick = () => {
+
+        contactBox.style.display = "flex";
+
+        setTimeout(function () {
+            contactBox.style.transform = "translate(0%)";
+        }, 50);
+
+
+    }
+
+    if (html.scrollTop < 1050) {
+        contactBox.style.transform = "translate(-100%)";
+        contactBox.style.display = "none";
+    }
 
 
 
+    // Type Effect 
+
+    var i = 0;
+    var txt = 'Visualiser. Executer. Developer.'; /* The text */
+    var speed = 100; /* The speed/duration of the effect in milliseconds */
+
+    function typeWriter() {
+        if (i < txt.length) {
+            document.getElementById("typeText").innerHTML += txt.charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+
+            typeText.style.height = "100%";
+            typeText.style.opacity = "1";
+        }
+    }
+
+    typeWriter();
+
+
+
+    const projectOneText = document.getElementById("projectOneText");
+    const projectTwoText = document.getElementById("projectTwoText");
+    const projectThreeText = document.getElementById("projectThreeText");
+    const projectFourText = document.getElementById("projectFourText");
+    const projectFiveText = document.getElementById("projectFiveText");
+
+
+    if (window.innerWidth > 930) {
+
+
+        projectOneText.onmouseover = () => {
+
+            setTimeout(() => {
+                projectOneText.style.width = "100%";
+
+            }, 50);
+    
+        }
+
+        projectOneText.onmouseout = () => {
+
+            setTimeout(() => {
+                projectOneText.style.width = "30%";
+            }, 50);
+
+        }
+
+        projectTwoText.onmouseover = () => {
+
+            setTimeout(() => {
+                projectTwoText.style.width = "100%";
+
+            }, 50);
+
+
+        }
+
+        projectTwoText.onmouseout = () => {
+
+            setTimeout(() => {
+                projectTwoText.style.width = "30%";
+            }, 50);
+
+        }
+
+        projectThreeText.onmouseover = () => {
+
+            setTimeout(() => {
+                projectThreeText.style.width = "100%";
+
+            }, 50);
+
+
+        }
+
+        projectThreeText.onmouseout = () => {
+
+            setTimeout(() => {
+                projectThreeText.style.width = "30%";
+            }, 50);
+
+        }
+
+
+        projectFourText.onmouseover = () => {
+
+            setTimeout(() => {
+                projectFourText.style.width = "100%";
+
+            }, 50);
+
+
+        }
+
+        projectFourText.onmouseout = () => {
+
+            setTimeout(() => {
+                projectFourText.style.width = "30%";
+            }, 50);
+
+        }
+
+        projectFiveText.onmouseover = () => {
+
+            setTimeout(() => {
+                projectFiveText.style.width = "100%";
+
+            }, 50);
+
+
+        }
+
+        projectFiveText.onmouseout = () => {
+
+            setTimeout(() => {
+                projectFiveText.style.width = "30%";
+            }, 50);
+
+        }
+
+
+    }
 
 
 
